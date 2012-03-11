e62ec00baeffb3ee02119d1563c99ac6545b6f04
Nathan Doss <ndoss@ndoss-MS-7681.(none)>
2012-03-11 13:16:56 -0400

    Tinkering around

--- water/main.lua
+++ water/main.lua
@@ -4,14 +4,6 @@ local bins = 20
 local layers = 3
 local texture = Texture.new("water.jpg", true)
 
---[[
-w = Water.new{ x=120, y=60, width=200, height=80, bezier=true, bins=bins, lineStyle = { 4 }}
-w = Water.new{ x=360, y=60, width=200, height=80, bezier=true, bins=bins, fillStyle = { Shape.TEXTURE, texture, Matrix.new(1,0,0,1,0,-300) }}
-w = Water.new{ x=120, y=260, width=200, height=80, bezier=true, velocityScale=20, viscosity=0.01, damping=0.98, bins=bins, fillStyle = { Shape.TEXTUREw = Water.new{ x=360, y=260, width=200, height=80, bezier=true, viscosity=0.995, damping=0.99, bins=bins, fillStyle = { Shape.SOLID, 0x777777, 0.3 }, 
-w = Water.new{ x=360, y=160, width=200, height=80, bezier=true, bins=bins, fillStyle = { Shape.NONE }, lineStyle = { 8, 0x00ff00, 0.5 }}
-w = Water.new{ x=120, y=160, width=200, height=80, bezier=true, bins=bins, fillStyle = { Shape.SOLID, 0xff0000, 0.5 }, lineStyle = { 1 }}
---]]
-
 -- Add the water
 math.randomseed(os.time())
 list = {}
@@ -21,7 +13,8 @@ for i=1,layers do
       fillStyle = { Shape.TEXTURE, texture, Matrix.new(1,0,0,1,0,-100) }
    end
    local w = Water.new{ 
-      bins=bins, 
+      bezier    = true,
+      bins      = bins, 
       fillStyle = fillStyle
    }
    stage:addChild(w)
@@ -36,13 +29,12 @@ local function addVelocity(event)
    local ratio = (y / application:getContentHeight() - 0.5);
    local vel   = ratio * maxVel
    for i,w in ipairs(list) do 
-      w:setVelocity(bins * x/application:getContentWidth(), vel * math.random(0,2), math.random(3,6))
+      w:setVelocity(bins * x/application:getContentWidth(), vel * math.random()*2, math.random(3,6))
    end
 end
 stage:addEventListener(Event.MOUSE_DOWN, addVelocity)
 stage:addEventListener(Event.MOUSE_MOVE, addVelocity)
 stage:addEventListener(Event.MOUSE_UP,   addVelocity)
-
 
 -- Add velocity based on timer 
 local width     = application:getContentWidth()
